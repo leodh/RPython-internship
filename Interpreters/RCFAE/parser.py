c1adbaa9207ecc5a2898ecca0bff7d723233afcc
Zebign1 <leonard.de.haro@ens.fr>
2012-08-16 17:26:20 -0400

    adding tree classes

--- Interpreters/RCFAE/parser.py
+++ Interpreters/RCFAE/parser.py
@@ -2,15 +2,48 @@
 
 import py
 from pypy.rlib.parsing.ebnfparse import parse_ebnf, make_parse_function
-
+from pypy.rlib.parsing.tree import Node, Nonterminal
 
 grammar = py.path.local().join('grammar.txt').read("rt")
 regexs, rules, ToAST = parse_ebnf(grammar)
 _parse = make_parse_function(regexs, rules, eof=True)
 
+# Tree classes
+
+class RCFAE(object):
+    """ For ineritance purpose only """
+    
+    def __init__(self):
+        pass
+
+    def __str__(self):
+        pass
+
+class Num(RCFAE):
+
+    def __init__(self, val):
+        self.val = val
+
+    def __str__(self):
+        return ("( Num : % )" % self.val)
+
+class Op(RCFAE):
+
+    def __init__(self, op, lhs, rhs):
+        self.op = op
+        self.lhs = lhs
+        self.rhs = rhs
+
+    def __str__(self):
+        return ("Op : % % %" % (self.lhs.__str__, self.op, self.rhs.__str__))
+
+
+
+# Main instructions
 
 def Main(source):
-    x = _parse(source)
+    tree = _parse(source)
+    print(tree.__str__())
     print(42)
 
 import os
