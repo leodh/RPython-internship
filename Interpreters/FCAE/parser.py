867a865128f4323458af60980f8a92f856de9816
Zebign1 <leonard.de.haro@ens.fr>
2012-08-16 16:25:12 -0400

    getting parser ready for first tests

--- Interpreters/FCAE/parser.py
+++ Interpreters/FCAE/parser.py
@@ -3,3 +3,42 @@
 import py
 from pypy.rlib.parsing.ebnfparse import parse_ebnf, make_parse_function
 
+
+grammar = py.path.local().join('grammar.txt').read("rt")
+regexs, rules, ToAST = parse_ebnf(grammar)
+_parse = make_parse_function(regexs, rules, eof=True)
+
+
+def Main(source):
+    x = _parse(source)
+    print(42)
+
+import os
+
+def run(fp):
+    program_envents = ""
+    while True:
+        read = os.read(fp, 4096)
+        if len(read) == 0:
+            break
+        program_envents += read
+    os.close(fp)
+    Main(program_envents)
+
+def entry_point(argv):
+    try:
+        filename = argv[1]
+    except IndexError:
+        print "You must supply a filename"
+        return 1
+
+    run(os.open(filename, os.O_RDONLY, 0777))
+    return 0
+
+
+def target(*args):
+    return entry_point, None
+
+if __name__ == "__main__":
+    entry_point(sys.argv)
+
